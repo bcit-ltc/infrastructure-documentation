bc7650ddc6d1660633a4afd1d50ab8933ced7223
Viresh Soedhwa <vsoedhwa@bcit.ca>
2025-03-24 09:17:33 -0700

    docker build from dockerfile

--- dagger/src/infrastructure_documentation/main.py
+++ dagger/src/infrastructure_documentation/main.py
@@ -1,7 +1,9 @@
 import random
 
+from typing import Annotated
+
 import dagger
-from dagger import dag, function, object_type
+from dagger import Doc, DefaultPath, dag, function, object_type
 
 
 @object_type
@@ -9,18 +11,21 @@ class InfrastructureDocumentation:
     @function
     async def publish(self, source: dagger.Directory) -> str:
         """Publish the application container after building and testing it on-the-fly"""
-        await self.test(source)
-        return await self.build(source).publish(
-            f"ttl.sh/infrastructure-documentation-{random.randrange(10**8)}"
+        
+        image = await self.dockerbuild(source)
+        ete_result = await self.ete_testing(image)
+        
+        return await image.publish(
+            f"infrastructure-documentation-{random.randrange(10**8)}"
         )
 
     @function
     def build(self, source: dagger.Directory) -> dagger.Container:
         """Build the application container"""
         build = (
-            self.build_env(source)
-            .with_exec(["mkdocs", "build", "--site-dir", "./output"])
-            .directory("./output")
+            self.install_dep(source)
+            .with_exec(["mkdocs", "build", "--site-dir", "./public"])
+            .directory("./public")
         )
         return (
             dag.container()
@@ -30,16 +35,84 @@ class InfrastructureDocumentation:
         )
     
     @function
-    async def test(self, source: dagger.Directory) -> str:
+    def dev(self, source: Annotated[dagger.Directory, DefaultPath("./")]) -> dagger.Container:
+        """Return the result of running unit tests"""
+        # directory = dagger.Directory("./")
+        return (
+            # self.install_dep(source)
+            dag.container()
+            .from_("squidfunk/mkdocs-material")
+            .with_mounted_directory("/docs", source)
+            # .with_mounted_cache("/root/.npm", node_cache)
+            .with_workdir("/docs")
+            .with_exec(["pip", "install", "Pygments", "pymdown-extensions", "mkdocs-git-revision-date-localized-plugin"])
+            # .with_exec(["mkdocs", "serve", "-a", "0.0.0.0:8000"])
+            .with_exposed_port(8000)
+            .as_service(args=["mkdocs", "serve", "-a", "0.0.0.0:8000"])
+            # .stdout()
+        )
+    
+    @function
+    def devdev(self, source: dagger.Directory) -> dagger.Container:
+        """Return the result of running unit tests"""
+        return (
+             dag.container()
+            .from_("node:alpine3.21")
+            .with_exec(["npm", "init", "-y"])
+            .with_exec(["npx", "create-react-app@latest", "myapp"])
+            .with_workdir("/myapp")
+            .with_exec(["npm", "start"])
+            .with_exposed_port(8000)
+            .stdout()
+        )
+
+    # 
+    @function
+    async def ete_testing(self, src: dagger.Container) -> None:
+        """Return the result of running end-to-end tests"""
+        
+        return await (
+            self.dockerbuild(src)
+            .with_exec(["cypress", "some ete testing command"])
+            .stdout()
+        )
+
+    # using a Dockerfile to build and return a container
+    @function
+    async def dockerbuild(
+        self,
+        src: Annotated[
+            dagger.Directory,
+            DefaultPath("./")
+        ],
+    ) -> dagger.Container:
+        """Build and image from existing Dockerfile"""
+        ref = await src.docker_build()
+        return await (
+            ref.with_exposed_port(8080)
+        )
+    
+    @function
+    def somenginx(self, source: dagger.Directory) -> dagger.Container:
+        """Return the result of running unit tests"""
+        return (
+            dag.container()
+            .from_("nginx:1.25-alpine")
+            # .with_directory("/usr/share/nginx/html", build)
+            .with_exposed_port(80)
+        )
+    
+    @function
+    async def unit_testing(self, source: dagger.Directory) -> str:
         """Return the result of running unit tests"""
         return await (
-            self.build_env(source)
+            self.install_dep(source)
             .with_exec(["mkdocs", "get-deps"])
             .stdout()
         )
 
     @function
-    def build_env(self, source: dagger.Directory) -> dagger.Container:
+    def install_dep(self, source: dagger.Directory) -> dagger.Container:
         """Build a ready-to-use development environment"""
         node_cache = dag.cache_volume("node")
         return (
