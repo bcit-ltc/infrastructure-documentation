cfaf6f767f1bccc2598987cc23da214938463a55
Viresh Soedhwa <vsoedhwa@bcit.ca>
2025-03-27 14:26:10 -0700

    fix directory args

--- dagger/src/infrastructure_documentation/main.py
+++ dagger/src/infrastructure_documentation/main.py
@@ -64,7 +64,7 @@ class InfrastructureDocumentation:
             .from_("ghcr.io/bcit-ltc/semantic-release:latest")  # Use prebuilt semantic-release container
             # Run semantic-release
             .with_workdir("/app")
-            .directory("/app", "./")
+            .directory("/app", source)
             .with_exec(["ls", "-la"])
             .with_exec(["npx", "semantic-release"])
         )
