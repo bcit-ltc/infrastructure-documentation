32e9563181228fe34cc060f9dee40454c0c2b2ce
Arvin Rolos <arolos@bcit.ca>
2025-08-07 10:07:19 -0700

    add nginx.conf

--- .devcontainer/devcontainer-setup.sh
+++ .devcontainer/devcontainer-setup.sh
@@ -20,7 +20,10 @@ echo "=== Building app Docker image ==="
 docker build -t k3d-localregistry.localhost:5000/myapp:latest .
 
 echo "=== Pushing image to local registry ==="
-docker push k3d-localregistry.localhost:5000/myapp:latest
+docker push k3d-localregistry.localhost:5000/myapp:latest.
+
+echo "=== Creating nginx configmap ==="
+kubectl create configmap myapp-nginx-config --from-file=.devcontainer/nginx.conf --dry-run=client -o yaml | kubectl apply -f -
 
 echo "=== Deploying app to k3d cluster ==="
 kubectl apply -f .devcontainer/deployment.yaml || { echo "Failed to apply deployment"; exit 1; }
