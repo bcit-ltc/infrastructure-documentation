910b513609f14d3b08179992816ca8d815f420b7
kphunter <kyle_hunter@bcit.ca>
2025-08-16 23:18:37 -0700

    changes location of app-chart (#36)

--- .devcontainer/scripts/app-chart.sh
+++ .devcontainer/scripts/app-chart.sh
@@ -6,14 +6,14 @@ SCRIPT_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 . "$SCRIPT_DIR/env.sh"
 
 # Retrieve app chart and move into location for skaffold
-if [ -d ".devcontainer/app-chart" ]; then
-  echo "⚠️ Directory .devcontainer/app-chart already exists. Skipping chart pull and move."
+if [ -d "app-chart" ]; then
+  echo "⚠️ Directory app-chart already exists. Skipping chart pull and move."
 else
   echo "📡 Retrieving and unpacking app helm chart..."
 
   # Assuming public access
-  helm pull oci://ghcr.io/$ORG_NAME/oci/$APP_ID --untar --destination .devcontainer
-  mv ".devcontainer/$APP_ID" ".devcontainer/app-chart"
+  helm pull oci://ghcr.io/$ORG_NAME/oci/$APP_ID --untar
+  mv "$APP_ID" "app-chart"
 fi
 
 echo -e "\n✅ Chart retrieval complete.\n"
